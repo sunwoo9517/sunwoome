5496d8d2c67b03f3b047c2b075775ee9d3783baf
sunwooJung <sunwoojung517@gmail.com>
2017-08-05 14:05:41 +0900

    sunwoome homepage initialized

---
+++ js/script.js
@@ -0,0 +1,207 @@
+$(function(){
+
+  var $banner = $('#banner'),
+    $header = $('#header'),
+    $section = $('section'),
+    $gallery = $('#gallery');
+
+  function animateRotate(node, angle){
+    var $elem = $(node);
+    $({deg:0}).animate({deg:angle},{
+      duration: 1300,
+      step: function(now){
+        $elem.css({
+          transform: 'rotate('+ now +'deg)'
+        });
+      }
+    });
+  };
+
+  jQuery.fn.extend({
+        rotator: function(child) {
+          if(!child){
+            child = $(this).children(':first');
+          }
+          $(child).fadeIn(600,function(){
+            $(child).delay(300).fadeOut(600,function(){
+              var next = child.next();
+              if(next.length === 0){
+                $(this).parent().rotator();
+              };
+              $(this).rotator(next);
+            });
+          });
+        }
+    });
+
+  $('#rotate').rotator();
+
+
+  //Alternative header
+  jQuery.fn.extend({
+    headerAlter : function(options){
+      var settings = $.extend({
+        backgroundColor: '#7fa00e',
+        position: 'relative',
+        top: null,
+        left: null,
+        right: null,
+        bottom: null,
+        width: '100%',
+        height: '2.7rem',
+        lineHeight: '2.7rem',
+        textAlign: 'right',
+        paddingTop: null,
+        paddingRight: '20px',
+        paddingBottom: null,
+        paddingLeft: null,
+        marginTop: null,
+        marginRight: null,
+        marginBottom: null,
+        marginLeft: null,
+        display: 'block',
+        animation: false,
+        zIndex: null
+      }, options);
+
+      var arr = ['top','left','right','bottom'];
+      for(var i=0;i<arr.length;i++){
+        var val = arr[i];
+        if(settings[val]){
+          $header.css(val, settings[val]);
+        }
+      }
+
+      var padding = {'paddingTop':'padding-top', 'paddingRight':'padding-right', 'paddingBottom':'padding-bottom', 'paddingLeft':'padding-left'};
+      jQuery.each(padding, function(i, val){
+        if(settings[i]){
+          $header.css(val, settings[i])
+        }
+      })
+
+      var margin = {'marginTop':'margin-top', 'marginRight':'margin-right', 'marginBottom':'margin-bottom', 'marginLeft':'margin-left'};
+      jQuery.each(margin, function(i, val){
+        if(settings[i]){
+          $header.css(val, settings[i]);
+        }
+      })
+
+      if(settings.animation){
+        $(this).hide().animate( { height: settings.height }, 800,'linear' );
+      }
+
+      if(settings.zIndex){
+        $(this).css('z-index', settings.zIndex)
+      }
+      if(settings.maxHeight){
+        $(this).css('max-height', settings.maxHeight)
+      }
+      if(settings.textAlign){
+        $(this).css('text-align', settings.textAlign)
+      }
+      //default
+      $(this)
+        .css('background-color',settings.backgroundColor)
+        .css('height',settings.height)
+        .css('width',settings.width)
+        .css('display',settings.display)
+        .css('position', settings.position)
+        .css('line-height', settings.lineHeight)
+    }
+  });
+
+  $(".btn").button({
+    classes:{
+      "ui-button": "ui-corner-all"
+    }
+  });
+
+  $banner.scrollex({
+    bottom: $header.outerHeight(),
+    enter: function(){
+      $header.headerAlter({
+        backgroundColor: 'transparent',
+        position: 'absolute',
+        top: '1.5rem',
+        right: '0px',
+        width:'100%',
+        height: '2.7rem',
+        zIndex: 2,
+        display: 'block'
+      });
+      $(this).find(".inner").animate({opacity:1}, 1500);
+    },
+    leave: function(){
+      $header.headerAlter({
+        backgroundColor: 'rgba(0, 0, 0, 0.75)',
+        display: 'inline-block',
+        position: 'fixed',
+        top:'0px',
+        right:'0px',
+        height: '2.7rem',
+        width: '100%',
+        zIndex: 2,
+        animation: true
+      });
+    }
+  });
+  $gallery.scrollex({
+    initialize: function(){
+      $header.headerAlter({
+        backgroundColor: '#1f4f58',
+        display: 'block',
+      });
+    }
+  })
+  $section.scrollex({
+    bottom: $header.outerHeight(),
+    initialize: function(){
+      $(this).addClass('inactive');
+      $(this).fadeIn(800);
+    },
+    enter: function(){
+      if($(this)[0].id == "two")
+        {animateRotate('#two img',720);}
+      if($(this)[0].id == "three")
+        {animateRotate('#three img',1080);}
+      $(this).removeClass('inactive');
+      $(this).find('.inner').animate({opacity:1},800);
+      $(this).find('.icon-up-open').animate({bottom:60,opacity:1},2500);
+    },
+    leave: function(){
+      $(this).addClass('inactive');
+      $(this).find('.inner').animate({opacity:0.1},800);
+      $(this).find('.icon-up-open').animate({bottom:0,opacity:0},600);
+    }
+  });
+
+  $(window).on('load', function(){
+    $('.thumbnails').poptrox({
+      usePopupCaption: true,
+      onPopupClose: function() { $gallery.removeClass('covering'); },
+			onPopupOpen: function() { $gallery.addClass('covering'); },
+      baseZIndex: 10001,
+      useBodyOverflow: false,
+      usePopupEasyClose: true,
+      overlayOpacity: 0.45,
+      popupLoaderText: '',
+      fadeSpeed: 500,
+      usePopupDefaultStyling: false,
+      overlayColor: '#999'
+    });
+
+    $('#four .flex').poptrox({
+      onPopupClose: function() { $('body').removeClass('covering'); },
+			onPopupOpen: function() { $('body').addClass('covering'); },
+      baseZIndex: 1001,
+      userBodyOverflow: false,
+      usePopupEasyClose: true,
+      overlayOpacity: 0.45,
+      popupLoaderText:'',
+      fadeSpeed: 500,
+      usePopupDefaultStyling: false,
+      overlayColor: '#999'
+    });
+
+  });
+});
---
+++ php/gallery.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="description" content="my homepage">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>SUNWOOme</title>
+  <link rel="stylesheet" href="../css/reset.css">
+  <link rel="stylesheet" href="../css/style.css">
+</head>
+  <body id="gallery">
+    <!-- header -->
+    <header id="header">
+      <div id="logo"><a href="../index.html"><span>Hello world</span> by SUNWOOme</a></div>
+      <!-- <a href="./php/board.php">Board</a> -->
+      <a href="../index.html">Home</a>
+    </header>
+    <!-- nav -->
+    <nav>
+      <ul>
+        <li><a href="#"></a></li>
+      </ul>
+    </nav>
+    <div id="wrapper">
+      <!-- main -->
+      <div id="main">
+        <!-- pictures -->
+        <div class="thumbnails">
+          <div>
+            <a href="../images/fulls/1.jpg">
+              <img src="../images/fulls/1.jpg" title="photo_1" alt="photo">
+              <h3>Photo by national geographic</h3>
+            </a>
+            <a href="../images/fulls/2.jpg">
+              <img src="../images/fulls/2.jpg" title="photo_2" alt="photo">
+              <h3>Photo by national geographic</h3>
+            </a>
+            <a href="../images/fulls/3.jpg">
+              <img src="../images/fulls/3.jpg" title="photo_3" alt="photo">
+              <h3>Photo by national geographic</h3>
+            </a>
+          </div>
+          <div>
+            <a href="../images/fulls/4.jpg">
+              <img src="../images/fulls/4.jpg" title="photo_4" alt="photo">
+              <h3>Photo by national geographic</h3>
+            </a>
+            <a href="../images/fulls/5.jpg">
+              <img src="../images/fulls/5.jpg" title="photo_5" alt="photo">
+              <h3>Photo by national geographic</h3>
+            </a>
+            <a href="../images/fulls/6.jpg">
+              <img src="../images/fulls/6.jpg" title="photo_6" alt="photo">
+              <h3>Photo by national geographic</h3>
+            </a>
+          </div>
+          <div>
+            <a href="../images/fulls/7.jpg">
+              <img src="../images/fulls/7.jpg" title="photo_7" alt="photo">
+              <h3>Photo by national geographic</h3>
+            </a>
+            <a href="../images/fulls/8.jpg">
+              <img src="../images/fulls/8.jpg" title="photo_8" alt="photo">
+              <h3>Photo by national geographic</h3>
+            </a>
+            <a href="../images/fulls/9.jpg">
+              <img src="../images/fulls/9.jpg" title="photo_9" alt="photo">
+              <h3>Photo by national geographic</h3>
+            </a>
+          </div>
+
+
+        </div>
+
+      </div>
+      <!-- footer -->
+      <footer>
+        This is a footer.
+      </footer>
+
+    </div>
+    <!-- Script -->
+    <script type="text/javascript" src="../js/jquery-2.2.4.min.js"></script>
+    <script type="text/javascript" src="../js/jquery.scrollex.min.js"></script>
+    <script type="text/javascript" src="../js/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="../js/jquery.poptrox.min.js"></script>
+    <script type="text/javascript" src="../js/script.js"></script>
+  </body>
+</html>
